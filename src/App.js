9a2a0e4f277e663e5e50eae75ec835914f0f164b
Ana Cunha <1771610+anacunha@users.noreply.github.com>
2022-09-16 16:20:43 -0300

    Create new Decks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import { DataStore } from 'aws-amplify';
+import { Deck } from './models';
 
 function App() {
+  const createDeck = async () => {
+    const newDeck = await DataStore.save(new Deck({
+      name: prompt("Deck name")
+    }))
+    console.log(newDeck);
+  }
   return (
     <div className="App">
+      <button onClick={createDeck}>Create Deck</button>
     </div>
   );
 }
