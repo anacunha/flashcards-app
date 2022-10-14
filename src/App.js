a7d4f609fc8e9ba570ef16ea2870e6646ae7024a
Ana Cunha <1771610+anacunha@users.noreply.github.com>
2022-10-13 21:01:43 -0300

    Use DeckCardCollection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import { NavBar } from './ui-components';
-import { withAuthenticator } from "@aws-amplify/ui-react";
+import { DeckCardCollection, NavBar } from './ui-components';
+import { Flex, View, withAuthenticator } from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar width='100vw' />
+      <Flex
+        direction="row"
+        justifyContent="space-around"
+      >
+        <View>
+          <DeckCardCollection overrideItems={({ item }) => ({
+            overrides: {
+              Cards: {
+                children: `${item.cards.length} ${item.cards.length === 1 ? 'card' : 'cards'}`
+              }
+            }
+          })} />
+        </View>
+      </Flex>
     </div>
   );
 }
