d8510e64be964ffc73fa68c07428675654d2cbff
Ana Cunha <1771610+anacunha@users.noreply.github.com>
2022-10-14 02:35:56 -0300

    Add Convert Predictions (text to speech)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,59 @@
+import { Amplify, Predictions } from 'aws-amplify';
+import { AmazonAIPredictionsProvider } from '@aws-amplify/predictions';
 import { CreateDeck, DeckCardCollection, FlashcardCollection, NavBar } from './ui-components';
 import { Flex, View, withAuthenticator } from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css';
 import { useState } from 'react';
+
+Predictions.addPluggable(new AmazonAIPredictionsProvider());
+
+function TextToSpeech() {
+  const [response, setResponse] = useState("...")
+  const [textToGenerateSpeech, setTextToGenerateSpeech] = useState("write to speech");
+
+  function generateTextToSpeech() {
+    setResponse('Generating audio...');
+    Predictions.convert({
+      textToSpeech: {
+        source: {
+          text: textToGenerateSpeech,
+          languageCode: 'pt-BR'
+        },
+        voiceId: 'Vitoria', // default configured on aws-exports.js
+        // list of different options are here https://docs.aws.amazon.com/polly/latest/dg/voicelist.html
+      }
+    }).then(result => {
+      let AudioContext = window.AudioContext || window.webkitAudioContext;
+      console.log({ AudioContext });
+      const audioCtx = new AudioContext();
+      const source = audioCtx.createBufferSource();
+      audioCtx.decodeAudioData(result.audioStream, (buffer) => {
+
+        source.buffer = buffer;
+        source.connect(audioCtx.destination);
+        source.start(0);
+      }, (err) => console.log({ err }));
+
+      setResponse(`Generation completed, press play`);
+    })
+      .catch(err => setResponse(err))
+  }
+
+  function setText(event) {
+    setTextToGenerateSpeech(event.target.value);
+  }
+
+  return (
+    <div className="Text">
+      <div>
+        <h3>Text To Speech</h3>
+        <input value={textToGenerateSpeech} onChange={setText}></input>
+        <button onClick={generateTextToSpeech}>Text to Speech</button>
+        <h3>{response}</h3>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   const [createDeck, setCreateDeck] = useState(false);
@@ -34,6 +86,7 @@ function App() {
           }
         </View>
         <View>
+          <TextToSpeech />
           {/* {editDeck ?
             <UpdateDeck deck={editDeck} overrides={{
               Close: { onClick: () => setEditDeck() }
